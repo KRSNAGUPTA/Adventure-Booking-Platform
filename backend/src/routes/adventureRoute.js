f42994bb4328869e32c04242e5e1214d3f6f13e0
KRSNAGUPTA <up.krishnagupta@gmail.com>
2025-01-25 00:35:28 +0530

    Add booking routes for creating, retrieving, and canceling bookings with authentication

--- backend/src/routes/adventureRoute.js
+++ backend/src/routes/adventureRoute.js
@@ -8,6 +8,7 @@ import {
 } from "../controller/adventureController.js";
 import { protect } from "../middlewares/authMiddleware.js";
 import adminMiddleware from "../middlewares/adminMiddleware.js";
+import { cancelBooking, createBooking, getBooking } from "../controller/bookingController.js";
 const router = Router();
 router.get("/", (req, res) => {
   res.send("API is running....");
@@ -22,4 +23,8 @@ router.delete(
   adminMiddleware,
   deleteAdventure
 );
+router.get("/booking",protect, getBooking);
+router.get("/bookingbyid",protect, getAdventureById);
+router.post("/booking/create",protect, createBooking)
+router.delete("/booking/cancel",protect, cancelBooking)
 export default router;

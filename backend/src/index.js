4bf5eac21f0762000e829837ceabd63d20f05440
KRSNAGUPTA <up.krishnagupta@gmail.com>
2025-01-24 23:29:31 +0530

    Add adventure routes for managing adventure-related API endpoints

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import connectDB from "./config/db.js";
 import userRoute from "./routes/userRoutes.js"
+import adventureRoute from "./routes/adventureRoute.js"
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -16,6 +17,7 @@ app.get("/", (req, res) => {
   res.send("API is running....");
 });
 app.use("/api/user", userRoute);
+app.use("/api/adventure", adventureRoute);
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);

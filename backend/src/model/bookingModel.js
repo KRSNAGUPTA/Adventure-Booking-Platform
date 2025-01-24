aa7fe081c75078c845b45b1833df5647975a8e5b
KRSNAGUPTA <up.krishnagupta@gmail.com>
2025-01-25 00:35:24 +0530

    Add totalPrice field to booking model for pricing information

--- backend/src/model/bookingModel.js
+++ backend/src/model/bookingModel.js
@@ -1,9 +1,17 @@
 import mongoose from "mongoose";
 const bookingSchema = new mongoose.Schema({
-    adventure: { type: mongoose.Schema.Types.ObjectId, ref: "Adventure", required: true },
-    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    date: { type: Date, required: true },
-    participants: { type: Number, required: true },
+  adventure: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Adventure",
+    required: true,
+  },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  date: { type: Date, required: true },
+  participants: { type: Number, required: true },
+  totalPrice: {
+    type: Number,
+    required: true,
+  },
 });
 
 export default mongoose.model("Booking", bookingSchema);

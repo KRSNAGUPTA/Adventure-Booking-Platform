eddebb37de41f2f818fb20ce3e0ebd8ab060a203
KRSNAGUPTA <up.krishnagupta@gmail.com>
2025-01-24 22:22:17 +0530

    Add booking model with schema for adventure, user, date, and participants

---
+++ backend/src/model/bookingModel.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+const bookingSchema = new mongoose.Schema({
+    adventure: { type: mongoose.Schema.Types.ObjectId, ref: "Adventure", required: true },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    date: { type: Date, required: true },
+    participants: { type: Number, required: true },
+});
+
+export default mongoose.model("Booking", bookingSchema);

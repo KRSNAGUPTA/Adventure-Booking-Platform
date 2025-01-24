8b0eee3208c69dda870957aa6a8c4cc721ef8d40
KRSNAGUPTA <up.krishnagupta@gmail.com>
2025-01-24 22:21:54 +0530

    Add user routes and a basic API endpoint

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import connectDB from "./config/db.js";
+import userRoute from "./routes/userRoutes.js"
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -11,7 +12,10 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
+app.get("/", (req, res) => {
+  res.send("API is running....");
+});
+app.use("/api/user", userRoute);
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
